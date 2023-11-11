38aae495c163f63553ba6012ce3dd4f66b8b8c1d
nils-mathieu <nils.mathieu.contact@gmail.com>
2023-11-11 17:05:29 +0100

    remove stray type

--- src/collections/mod.rs
+++ src/collections/mod.rs
@@ -1,19 +1,4 @@
 //! Collections.
 
-use core::fmt;
-
 pub mod read_buffer;
 pub use self::read_buffer::ReadBuffer;
-
-/// An error that occurs when an allocation fails.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct OutOfMemory;
-
-impl fmt::Display for OutOfMemory {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.pad("out of memory")
-    }
-}
-
-/// The result type for memory-backed collections.
-pub type Result<T> = ::core::result::Result<T, OutOfMemory>;
