7efabc6a9a1a84d157f610e6acf39f3ca02acc1d
nils-mathieu <nils.mathieu.contact@gmail.com>
2023-11-26 17:22:36 +0100

    add lock debug info

--- src/sync/mutex/noblock.rs
+++ src/sync/mutex/noblock.rs
@@ -1,5 +1,5 @@
-use core::sync::atomic::AtomicBool;
 use core::sync::atomic::Ordering::{Acquire, Relaxed, Release};
+use core::sync::atomic::{AtomicBool, AtomicPtr};
 
 use super::Lock;
 
@@ -9,36 +9,66 @@ const UNLOCKED: bool = true;
 /// A [`Lock`] implementation that never blocks.
 ///
 /// If a lock cannot be acquired, the lock panics instead.
-pub struct NoBlockLock(AtomicBool);
+pub struct NoBlockLock {
+    /// The location of the last lock operation.
+    #[cfg(debug_assertions)]
+    location: AtomicPtr<core::panic::Location<'static>>,
+    /// The current state of the lock.
+    state: AtomicBool,
+}
 
 unsafe impl Lock for NoBlockLock {
     #[allow(clippy::declare_interior_mutable_const)]
-    const UNLOCKED: Self = Self(AtomicBool::new(UNLOCKED));
+    const UNLOCKED: Self = Self {
+        #[cfg(debug_assertions)]
+        location: AtomicPtr::new(core::ptr::null_mut()),
+        state: AtomicBool::new(UNLOCKED),
+    };
 
     type GuardMarker = ();
 
     #[inline]
     fn is_locked(&self) -> bool {
-        self.0.load(Relaxed) == LOCKED
+        self.state.load(Relaxed) == LOCKED
     }
 
     #[track_caller]
     #[inline]
     fn lock(&self) {
         if !self.try_lock() {
+            #[cfg(debug_assertions)]
+            panic!("lock already taken at {:?}", unsafe {
+                *self.location.load(Relaxed)
+            });
+
+            #[cfg(not(debug_assertions))]
             panic!("lock already taken");
         }
     }
 
     #[inline]
+    #[allow(clippy::let_and_return)]
     fn try_lock(&self) -> bool {
-        self.0
+        let ret = self
+            .state
             .compare_exchange_weak(UNLOCKED, LOCKED, Acquire, Relaxed)
-            .is_ok()
+            .is_ok();
+
+        #[cfg(debug_assertions)]
+        if ret {
+            self.location.store(
+                core::panic::Location::caller() as *const _ as *mut _,
+                Relaxed,
+            );
+        }
+
+        ret
     }
 
     #[inline]
     unsafe fn unlock(&self) {
-        self.0.store(UNLOCKED, Release);
+        #[cfg(debug_assertions)]
+        self.location.store(core::ptr::null_mut(), Relaxed);
+        self.state.store(UNLOCKED, Release);
     }
 }
